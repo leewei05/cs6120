d2002d47e5f3fbae93f0fae37447ea6491424c16
Lee <lee10202013@gmail.com>
2024-03-04 21:23:32 +0800

    Add leibniz formula benchmark

--- l2/leibniz.py
+++ l2/leibniz.py
@@ -1,14 +1,14 @@
 def leibniz_pi():
     denom = 1
-    pi = 0;
+    pi = 0
 
     for i in range(1, 1000000):
         if (i % 2 == 0):
-            pi -= 1 / denom;
+            pi -= 1 / denom
         else:
-            pi += 1 / denom;
+            pi += 1 / denom
 
-        denom += 2;
+        denom += 2
 
     pi *= 4
     print(pi)
